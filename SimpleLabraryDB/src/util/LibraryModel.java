2f15a1d263b5c5d23aeaa2fe45f89af224ed84e7
Yan <alvindaiyan@gmail.com>
2012-10-02 13:04:14 +1300

    database connection

--- SimpleLabraryDB/src/util/LibraryModel.java
+++ SimpleLabraryDB/src/util/LibraryModel.java
@@ -9,14 +9,39 @@ package util;
 
 import javax.swing.*;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.SQLException;
+
 public class LibraryModel {
 
     // For use in creating dialogs and making them modal
     private JFrame dialogParent;
-
+    private Connection con = null;
+    
+    
     public LibraryModel(JFrame parent, String userid, String password) {
     	//connect to database
-    	System.out.println("I am tring to use github!!!");
+    	try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+    	
+    	String url = "jdbc:postgresql:" + "//db.ecs.vuw.ac.nz/" + userid + "_jdbc";
+    	
+    	try {
+			con = DriverManager.getConnection(url, userid, password);
+		} catch (SQLException e) {
+			// TODO cannot connect and print out the message
+			e.printStackTrace();
+		}
+    	
+    	
+    	
+    	
+    	//System.out.println("I am tring to use github!!!");
     	dialogParent = parent;
     }
 
